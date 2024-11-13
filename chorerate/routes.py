dcaedd1e0eeb416420c21beefdd2033c5ce1c42d
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-11-13 22:15:04 +0000

    Notify if password/username incorrect on login attempt

--- chorerate/routes.py
+++ chorerate/routes.py
@@ -150,10 +150,18 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password, password):
-            flash(f"Welcome {user.username}!", 'success')
-            login_user(user)
-            return redirect(url_for('homepage'))
+        if user:
+            if check_password_hash(user.password, password):
+                flash(f"Welcome {user.username}!", 'success')
+                login_user(user)
+
+                return redirect(url_for('homepage'))
+            else:
+                flash('Incorrect password.', 'danger')
+        else:
+            flash('User does not exist.', 'danger')
+
+        return redirect(url_for('login'))
     return render_template('login.html')
 
 
