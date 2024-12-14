359591f66e7e72281ee88e2b9cd08f4f50ef80d2
avabellaw <70448139+avabellaw@users.noreply.github.com>
2024-12-14 16:37:53 +0000

    Add check for env.py

--- chorerate/__init__.py
+++ chorerate/__init__.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
-import env # noqa - imports variables from env.py
+if os.path.exists("env.py"):
+    import env # noqa - imports variables from env.py
 
 app = Flask(__name__)
 
