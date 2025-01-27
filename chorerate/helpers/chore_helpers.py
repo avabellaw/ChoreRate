7ed83f680e4aad5cc62066415bda86779c904f58
avabellaw <avabellawilliams@gmail.com>
2025-01-27 16:40:50 +0000

    fix: Show the user's unrated household chores

--- chorerate/helpers/chore_helpers.py
+++ chorerate/helpers/chore_helpers.py
@@ -1,15 +1,23 @@
 
 from chorerate.models.chore import Chore
 from chorerate.models.chore_rating import ChoreRating
-from chorerate.helpers.household_helpers import current_household_member
+from chorerate.helpers.household_helpers import current_household_member, current_household
 from chorerate import db
 
 
 def get_unrated_from_db():
     '''Get unrated chores for the current user'''
-    current_member_id = current_household_member().id
-    rated = db.session.query(ChoreRating.chore_id).filter_by(
-        household_member_id=current_member_id)
-    unrated = db.session.query(Chore).filter(~Chore.id.in_(rated)).all()
+    current_member = current_household_member()
+    current_member_id = current_member.id
+    current_household_id = current_member.household_id
 
-    return unrated
+    # Subquery to find all chore IDs that the current user has rated
+    rated_chore_ids_subquery = db.session.query(ChoreRating.chore_id).filter_by(household_member_id=current_member_id).subquery()
+
+    # Find all unrated chores in the user's household
+    unrated_chores = db.session.query(Chore).filter(
+        Chore.household_id == current_household_id,
+        ~Chore.id.in_(rated_chore_ids_subquery)
+    ).all()
+
+    return unrated_chores
