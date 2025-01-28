98042eb8e4f7832c6e3e87cbd219e4ad88b7a975
avabellaw <avabellawilliams@gmail.com>
2025-01-28 15:25:09 +0000

    fix: Evaluation of DEBUG variable

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from chorerate import app
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', 5000)),
-            debug=os.environ.get('DEBUG', False))
+            debug=eval(os.environ.get('DEBUG', False)))
